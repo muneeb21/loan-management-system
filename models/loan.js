bdc38339662b500c966f4ef3ccf147b05d664f4b
Muneeb Adil <muneebadil07@gmail.com>
2020-10-18 13:20:35 +0530

    setup the project and add model schemas

---
+++ models/loan.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+
+
+const loanSchema = new mongoose.Schema({
+    
+    user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+    principle: {
+        type: String,
+        required: true
+    },
+    interest: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    
+    loanType: {
+        type: String,
+        required: true
+    },
+    timetorepay: {
+        type: Number,
+        required: true
+    },
+    collateral: {
+        type: String,
+        required: true
+    },
+    repaymentAmount: {
+        type: Number,
+        required: true
+    },
+    emi: {
+        type: Number,
+        required: true
+    },
+    status: {
+		type: String,
+		required: true,
+		enum: ["NEW", "REJECTED", "APPROVED"],
+	},
+    
+    
+}, {
+    timestamps: true
+});
+
+
+
+
+
+
+const Loan = mongoose.model('Loan', loanSchema);
+
+module.exports = Loan;
---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+
+
+const userSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userType: {
+        type: String,
+        required: true
+    },
+    loans: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Loan",
+		},
+	],
+    
+}, {
+    timestamps: true
+});
+
+
+
+
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
